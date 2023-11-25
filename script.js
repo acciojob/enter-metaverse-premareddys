4c567cd730f7d9dfc52bb64f4b739e809070a11c
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-25 20:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const metaverse= document.getElementById("status");
+const button= document.getElementById("enterBtn");
+function listener(){
+	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
+}
+
+button.addEventListener("click", listener);
